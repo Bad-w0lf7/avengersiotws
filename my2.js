1cac20b20365d38f6c319f54a5c0488ef34efb88
rohitkumarh94 <rohitkumarh94@gmail.com>
2015-06-27 11:19:15 +0530

    Update my2.js

--- my2.js
+++ my2.js
@@ -3,11 +3,9 @@ $("button").click(function(){
 $.post("https://data.sparkfun.com/input/RMbp8YVRQ2flJ7bo4z8w?private_key=lz7nVgKx0MCnNjY7gMwB",
 {
 temp: $("#test").val()
-},
-$.post("https://data.sparkfun.com/input/RMbp8YVRQ2flJ7bo4z8w?private_key=lz7nVgKx0MCnNjY7gMwB",
-{
 temp: $("#test1").val()
 },
+
 
 function(data){
 alert("Data: " + $("#test").val() + " sent to sparkfun");
